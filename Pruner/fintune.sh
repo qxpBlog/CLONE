698419e2f36fdb0d22ca76342b3c8c79c9d48d22
Xinpeng Qin <96739096+qxpBlog@users.noreply.github.com>
2025-05-07 23:04:50 +0800

    Add files via upload

---
+++ Pruner/fintune.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+HF_ENDPOINT=https://hf-mirror.com
+
+
+# fintune on alpaca
+HF_ENDPOINT=$HF_ENDPOINT CUDA_VISIBLE_DEVICES=0 python post_training.py \
+      --prune_model prune_log/llama_prune/your_pytorch_model.bin \
+      --data_path yahma/alpaca-cleaned \
+      --lora_r 8 \
+      --num_epochs 2 \
+      --learning_rate 1e-4 \
+      --batch_size 64 \
+      --output_dir tune_log_llama2/alpaca \
+      --wandb_project alpaca 
+
+# fintune on bbh
+HF_ENDPOINT=$HF_ENDPOINT CUDA_VISIBLE_DEVICES=0 python post_training.py \
+      --prune_model prune_log/llama_prune/your_pytorch_model.bin \
+      --data_path lukaemon/bbh \
+      --lora_r 8 \
+      --num_epochs 2 \
+      --learning_rate 1e-4 \
+      --batch_size 64 \
+      --output_dir tune_log_llama2/bbh \
+      --wandb_project bbh
