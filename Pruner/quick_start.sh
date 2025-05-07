08cbd013d4e5d2243641471b34f28e1ce768afb9
Xinpeng Qin <96739096+qxpBlog@users.noreply.github.com>
2025-05-07 23:59:48 +0800

    Update quick_start.sh

--- Pruner/quick_start.sh
+++ Pruner/quick_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 HF_ENDPOINT=https://hf-mirror.com
 BASE_MODEL=meta-llama/Llama-2-7b
-ORTHER_PRUNING_PATH=LLM-Pruner-main/eval_logs
+ORTHER_PRUNING_PATH=Pruner/eval_logs
 
 mkdir -p "$ORTHER_PRUNING_PATH"
 
@@ -19,14 +19,6 @@ HF_ENDPOINT=$HF_ENDPOINT CUDA_VISIBLE_DEVICES=1 python hf_prune.py \
       > "$ORTHER_PRUNING_PATH/test_latency_energy_0.2.log" 2>&1
 
 
-export PYTHONPATH='.' 
-HF_ENDPOINT=https://hf-mirror.com CUDA_VISIBLE_DEVICES=1 python lm-evaluation-harness/main.py \
-      --model hf-causal-experimental \
-      --model_args checkpoint=prune_log/llama_prune_0.2/pytorch_model.bin,config_pretrained=$BASE_MODEL \
-      --tasks openbookqa,arc_easy,winogrande,hellaswag,arc_challenge,piqa,boolq \
-      --no_cache \
-      > "$ORTHER_PRUNING_PATH/0.2_mlt.log" 2>&1
-
 echo "20% block fine-tuning"
 HF_ENDPOINT=$HF_ENDPOINT CUDA_VISIBLE_DEVICES=1 python post_training.py \
       --prune_model prune_log/llama_prune_0.2/pytorch_model.bin \
@@ -37,18 +29,3 @@ HF_ENDPOINT=$HF_ENDPOINT CUDA_VISIBLE_DEVICES=1 python post_training.py \
       --batch_size 64 \
       --output_dir tune_log_llama2/0.2 \
       --wandb_project 0.2 
-
-echo "fine-tuninged 20% block test latecy and energy"
-HF_ENDPOINT=$HF_ENDPOINT CUDA_VISIBLE_DEVICES=1 python test_latency_energy.py \
-      --ckpt prune_log/llama_prune_0.2/pytorch_model.bin \
-      --lora_ckpt tune_log_llama2/0.2 \
-      > "$ORTHER_PRUNING_PATH/fine-tuning_0.2_test_latency_energy.log" 2>&1
-
-
-export PYTHONPATH='.' 
-    HF_ENDPOINT=$HF_ENDPOINT CUDA_VISIBLE_DEVICES=1 python lm-evaluation-harness/main.py \
-        --model hf-causal-experimental \
-        --model_args checkpoint=prune_log/llama_prune_0.2/pytorch_model.bin,peft=tune_log_llama2/0.2,config_pretrained=$BASE_MODEL \
-        --tasks openbookqa,arc_easy,winogrande,hellaswag,arc_challenge,piqa,boolq \
-        --no_cache \
-        > "$ORTHER_PRUNING_PATH/fine-tuning_0.2_test_mlt.log" 2>&1
