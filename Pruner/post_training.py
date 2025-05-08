8f19caf4d28623d30c41fba9fd9558ea7055bc08
Xinpeng Qin <96739096+qxpBlog@users.noreply.github.com>
2025-05-08 12:05:58 +0800

    Update post_training.py

--- Pruner/post_training.py
+++ Pruner/post_training.py
@@ -30,7 +30,7 @@ def main(args):
     os.environ["WANDB_PROJECT"] = args.wandb_project
 
     # Load Pruned Model
-    pruned_dict = torch.load(args.prune_model, map_location='cuda')
+    pruned_dict = torch.load(args.prune_model, map_location='cuda', weights_only=False)
     tokenizer, model = pruned_dict['tokenizer'], pruned_dict['model']
 
     gradient_accumulation_steps = args.batch_size // args.micro_batch_size
